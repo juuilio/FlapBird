6129f5e4c1a8f6281042735c3b1517aea3200de8
Andre Alves Miguel <andre_alvesmiguel@hotmail.com>
2025-03-21 08:50:52 -0300

    update

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from random import randint
 import pygame
 pygame.init()
 x = 50
@@ -11,6 +12,16 @@ janela = pygame.display.set_mode((360,640))
 pygame.display.set_caption("Flappy Bird")
 
 janela_aberta = True
+canoInferior = pygame.image.load("src/assets/images/canoInferior.png")
+canoInferior = pygame.transform.scale(canoInferior, (80, 500))
+canoInferior_x = randint(360, 720) #15
+canoInferior_y = 0
+
+canoSuperior = pygame.image.load("src/assets/images/canoSuperior.png")
+canoSuperior = pygame.transform.scale(canoSuperior, (80, 500))
+canoSuperior_x = 360
+canoSuperior_y = 0
+
 while janela_aberta:
     pygame.time.delay(50)
 
@@ -19,13 +30,22 @@ while janela_aberta:
             janela_aberta = False
 
     comandos = pygame.key.get_pressed()
-    if comandos[pygame.K_SPACE]:
+
+    if comandos[pygame.K_SPACE] and y > 0:
         y -= 30
-    else:
+    elif y < 590:
         y += 10
+
+    canoInferior_x -= 10
+    if canoInferior_x < -80:
+        canoInferior_x = 360
+    if canoSuperior_x < -80:
+        canoSuperior_x = 360
 
     janela.blit(fundo, (0,0))
     janela.blit(passaro, (x,y))
+    janela.blit(canoInferior, (canoInferior_x, canoInferior_y))
+    janela.blit(canoSuperior, (canoInferior_x, canoSuperior_y))
 
     pygame.display.update()
 
